62b5b7f2b29237375b885f1593c3e2d82891bf71
Marina Panchenko <marishka100281@gmail.com>
2025-03-15 01:35:43 +0200

    rout

--- backend/server.mjs
+++ backend/server.mjs
@@ -34,3 +34,6 @@ const PORT = process.env.PORT || 3000;
 app.listen(PORT, "0.0.0.0", () => {
   console.log(chalk.blue.bold(`Server is running on port ${PORT}`));
 });
+app.get("/", (req, res) => {
+  res.send("Server is running! 🚀");
+});
