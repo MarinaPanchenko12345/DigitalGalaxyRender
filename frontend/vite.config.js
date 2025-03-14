7400910309444398e3eb630dedfbb97c4d5cf580
Marina Panchenko <marishka100281@gmail.com>
2025-03-14 22:40:09 +0200

    cancel port

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,6 +5,7 @@ export default defineConfig({
   plugins: [react()],
   base: "/",
   server: {
+    port: 5174,
     proxy: {
       "/api": {
         target: "https://digitalgalaxyrender.onrender.com",
