f121c6f3d3c5357e440c3098fe04314ca2278962
Marina Panchenko <marishka100281@gmail.com>
2025-03-15 08:22:02 +0200

    cors

--- backend/middlewares/cors.mjs
+++ backend/middlewares/cors.mjs
@@ -4,8 +4,9 @@ import { FRONTEND_URL } from '../helpers/config.mjs';
 //Middleware for CORS.
 // It specifies the allowed H5TTP methods and headers for cross-origin requests.
 export const corsMiddleware = cors({
-  origin: FRONTEND_URL,
+  origin: [FRONTEND_URL],
   credentials: true,
   methods: "GET,PUT,POST,PATCH,DELETE,OPTIONS",
   allowedHeaders: "Content-Type, Accept, Authorization",
+  preflightContinue: true,
 });
