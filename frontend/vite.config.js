bb9103c765dedcdeeac3221e664765d776bcb98e
Marina Panchenko <marishka100281@gmail.com>
2025-03-15 02:11:27 +0200

    Fix Vite redirects for React Router

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   base: "/",
   build: {
     outDir: "dist",
-    copyPublicDir: true, // Гарантирует копирование public/
+    copyPublicDir: true, 
   },
   server: {
     historyApiFallback: true,
