12162b5846777a03c15d3c90ed49661842fce8a1
Marina Panchenko <marishka100281@gmail.com>
2025-03-15 19:09:13 +0200

    change vite

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,18 +4,7 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   base: "/",
-  build: {
-    outDir: "dist",
-    copyPublicDir: true,
-  },
   server: {
     historyApiFallback: true,
-  },
-  resolve: {
-    fallback: {
-      fs: false,
-      path: false,
-      os: false,
-    },
   },
 });
