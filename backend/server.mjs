9d21b61528205d4d31e186092b42e4bd0dc1d802
Marina Panchenko <marishka100281@gmail.com>
2025-03-17 01:17:39 +0200

    app.head

--- backend/server.mjs
+++ backend/server.mjs
@@ -32,6 +32,9 @@ app.use((err, req, res, next) => {
 app.get("/", (req, res) => {
   res.send("Server is running!");
 });
+app.head("/", (req, res) => {
+  res.sendStatus(200);
+});
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, "0.0.0.0", () => {
